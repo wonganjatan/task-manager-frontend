9208740a4970c4810f8ce034fca7276ca2bb1661
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-19 13:52:55 +1100

    style: add login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,31 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Login() {
+    const [formData, setFormData] = useState({
+        username: "",
+        password: ""
+    })
+
+    function handleSubmit() {
+
+    }
+
     return (
-        <div class="">
-            <div class="">
-                <h2>Login</h2>
-                <form>
-                    <div class="form-group">
-                        <label for="username">Username:</label>
-                        <input type="text" id="username" name="username" required/>
+        <div className="flex h-screen items-center justify-center">
+            <div className="border rounded-2xl p-8 shadow-2xl">
+                <h2 className='text-2xl font-bold mb-4'>Login</h2>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                    <div className="flex flex-col">
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" name="username" className='border rounded-lg p-0.5' value={formData.username} onChange={e => setFormData({...formData, [e.target.name]: e.target.value})} required/>
                     </div>
-                    <div class="form-group">
-                        <label for="password">Password:</label>
-                        <input type="password" id="password" name="password" required/>
+                    <div className="flex flex-col">
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" name="password" className='border rounded-lg p-0.5' value={formData.password} onChange={e => setFormData({...formData, [e.target.name]: e.target.value})} required/>
                     </div>
 
-                    <button type="submit">Login</button>
+                    <button type="submit" className='border rounded-lg px-2 pb-1 bg-blue-500 text-white'>Login</button>
                 </form>
                 <p>
                     Do not have an account?{" "}
