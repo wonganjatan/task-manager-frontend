6338ad207739f8d6d5ffbb799777387cb3ec967c
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-20 17:07:13 +1100

    fix: task card description overlappting priority

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -4,7 +4,7 @@ function TaskCard({ task }) {
         <div className="flex flex-col justify-between border rounded-2xl w-60 h-60 p-4 shadow-2xl bg-linear-to-br from-indigo-200 via-purple-200 to-pink-200">
             <div>
                 <p className="text-xl font-bold">{task.title}</p>
-                <p className="h-12"><strong>Description: </strong>{task.description}</p>
+                <p className="h-18"><strong>Description: </strong>{task.description}</p>
                 <p><strong>Priority: </strong> {task.priority}</p>
                 <p><strong>Status: </strong>{task.status}</p>
                 <p>
