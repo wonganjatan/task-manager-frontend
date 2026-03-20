b67d5a5102310dfac642c70b10af63837f83d718
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-20 12:08:42 +1100

    feat: add Tasks component

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,39 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import TaskCard from "./TaskCard"
+
+function Tasks() {
+    const [data, setData] = useState({ tasks: [], tasksCount: 0 })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(
+                    "http://localhost:8080/api/admin/tasks"
+                )
+
+                setData(res.data)
+            } catch (error) {
+                
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    return (
+        <div className="p-8">
+            <div className="flex items-center justify-between pb-8">
+                <h1 className="text-4xl">Tasks ({data.tasksCount})</h1>
+                <p>Link to create Task</p>
+            </div>
+            <div className="flex flex-wrap gap-4">
+                {data.tasks.map(task => (
+                    <TaskCard key={task.id} task={task}/>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Tasks
