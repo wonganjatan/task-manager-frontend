a0e527013a90ef4a7aef930bb135c0616f79f768
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-20 12:09:02 +1100

    feat: add TaskCard component

---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,20 @@
+function TaskCard({ task }) {
+
+    return (
+        <div className="flex flex-col justify-between border rounded-2xl w-60 h-60 p-4 shadow-2xl bg-linear-to-br from-indigo-200 via-purple-200 to-pink-200">
+            <div>
+                <p className="text-xl">{task.title}</p>
+                <p>{task.description}</p>
+                <p><strong>Priority: </strong> {task.priority}</p>
+                <p><strong>Status: </strong>{task.status}</p>
+                <p><strong>Assigned to: </strong>{task.assignedUser.firstName + ' ' + task.assignedUser.lastName}</p>
+                <p><strong>Due: </strong>{task.dueDate}</p>
+            </div>
+            <div className="flex items-center justify-center">
+                <p>View</p>
+            </div>
+        </div>
+    )
+}
+
+export default TaskCard
