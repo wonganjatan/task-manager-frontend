b80aa2f8fefc28eaeb6fe5210361db1d14d49db5
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-21 20:00:27 +1100

    fix: bug where assigned user always null

--- src/pages/TaskCreatePage.jsx
+++ src/pages/TaskCreatePage.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
-import TaskForm from "../components/TaskForm"
 import { toast } from "react-toastify"
 import axios from "axios"
+import TaskForm from "../components/TaskForm"
+
 
 function TaskCreatePage() {
 
     const [users, setUsers] = useState([])
+    const buttonLabel = 'Create'
 
     useEffect(() => {
 
@@ -13,7 +15,6 @@ function TaskCreatePage() {
             try {
                 const response = await axios.get("http://localhost:8080/api/admin/users")
                 setUsers(response.data)
-                console.log(users)
             } catch (error) {
                 toast.error(error)
             }
@@ -24,10 +25,7 @@ function TaskCreatePage() {
 
     return (
         <div className="flex h-screen items-center justify-center">
-            <div className="border rounded-2xl p-8 shadow-2xl">
-                <h1 className="pb-8">Create Task</h1>
-                <TaskForm users={users}/>
-            </div>
+            <TaskForm users={users} buttonLabel={buttonLabel}/>
         </div>
     )
 }
