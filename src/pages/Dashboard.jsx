a373e754516c9e005a678d276e4313c98b05babb
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-22 14:00:45 +1100

    feat: add jwt aunthetication

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,34 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { toast } from "react-toastify"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 function Dashboard() {
-    const [tasksCount, setTasksCount] = useState(0)
+    const navigate = useNavigate()
+    const [totalTasks, setTotalTasks] = useState(0)
 
     useEffect(() => {
+        const token = localStorage.getItem("token")
+
         const fetchData = async () => {
             try {
-            const res = await axios.get(
-                "http://localhost:8080/api/admin/dashboard")
-
-            setTasksCount(res.data.tasksCount)
+                const response = await axios.get(
+                    "http://localhost:8080/api/admin/dashboard",
+                    { headers : {
+                        Authorization: `Bearer ${token}`
+                    }})
+                
+                if (response.status === 200) {
+                    setTotalTasks(response.data.totalTasks)
+                }
             } catch (error) {
-                toast.error("Error: getTasksCount")
+                if (error.response?.status === 401) {
+                    toast.error(error.response?.data?.message)
+                    
+                    return navigate("/")
+                } else {
+                    toast.error(error.response?.data?.message)
+                }
             }
         }
 
@@ -25,7 +39,7 @@ function Dashboard() {
         <div className="h-screen flex flex-col items-center justify-center">
             <div className="border rounded-2xl p-8">
                 <div>
-                    <h1 className="text-4xl">Tasks: {tasksCount}</h1>
+                    <h1 className="text-4xl">Tasks: {totalTasks}</h1>
                 </div>
                 <div className="flex items-center justify-center pt-4">
                     <Link to="/tasks" className='border rounded-lg px-2 pb-1 bg-blue-500 text-white'>Tasks</Link>
