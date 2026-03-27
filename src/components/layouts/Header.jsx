b29551a7556005b2563bb3aa0d09b34e8bfd1b6e
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-27 14:55:38 +1100

    feat: add role validation to the header

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,13 +1,27 @@
-import { Link } from "react-router-dom"
+import { jwtDecode } from "jwt-decode"
+import { Link, useNavigate } from "react-router-dom"
 
 function Header() {
+    const navigate = useNavigate()
+
+    const token = localStorage.getItem("token")
+
+    const handleSubmit = () => {
+        localStorage.removeItem("token")
+        navigate("/")
+    }
+
+    const decoded = jwtDecode(token)
+    const dashboard = decoded.role === "ADMIN" ? "/admin/dashboard" : "/user/dashboard"
+    const tasks = decoded.role === "ADMIN" ? "/admin/tasks" : "/user/tasks"
 
     return (
         <div className="flex items-center justify-between p-8 bg-gray-800">
             <h1 className="text-white text-2xl">Task Manager</h1>
             <div className="flex gap-8">
-                <Link to="/dashboard" className="text-white">Dashboard</Link>
-                <Link to="/tasks" className="text-white">Tasks</Link>
+                <Link to={dashboard} className="text-white">Dashboard</Link>
+                <Link to={tasks} className="text-white">Tasks</Link>
+                <button type="submit" onClick={handleSubmit}>Log out</button>
             </div>
         </div>
     )
