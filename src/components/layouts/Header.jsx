536a5d0fdcf1220bcfaabeff94beb1c4ca021b9d
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-28 18:00:01 +1100

    style(Header): add style to the Header component

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -19,9 +19,9 @@ function Header() {
         <div className="flex items-center justify-between p-8 bg-gray-800">
             <h1 className="text-white text-2xl">Task Manager</h1>
             <div className="flex gap-8">
-                <Link to={dashboard} className="text-white">Dashboard</Link>
-                <Link to={tasks} className="text-white">Tasks</Link>
-                <button type="submit" onClick={handleSubmit} className="text-white">Log out</button>
+                <Link to={dashboard} className="text-white flex justify-center rounded-x p-1 border-2 border-transparent hover:border-b-blue-500 transition-all duration-300">Dashboard</Link>
+                <Link to={tasks} className="text-white flex justify-center rounded-x p-1 border-2 border-transparent hover:border-b-green-500 transition-all duration-300">Tasks</Link>
+                <button type="submit" onClick={handleSubmit} className="text-white flex justify-center rounded-x p-1 border-2 border-transparent hover:border-b-yellow-500 transition-all duration-300 cursor-pointer">Log out</button>
             </div>
         </div>
     )
